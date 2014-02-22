f539bbbb2777e924157863b3a9e4cb258f3ece5d
Tiziano Basile <tiz.basile@gmail.com>
2014-02-22 09:40:00 +0100

    project updated

--- js/app.js
+++ js/app.js
@@ -1,18 +1,10 @@
-$(document).ready(function(){
-	var userJson;
-	$("#searchrepos").click(function(){
-		urlbase = "https://api.github.com/users/";
-		urlappend = "/repos";
-		username = $("#searchrepos").value;
-		$.get(urlbase + "tizionario" + urlappend, function(data){
-			userJson = data;
-			
-			//console.log(data);
-			$(userJson).each(function(index, value){
-				console.log(value.name);
-				console.log(value.description);
-				
-			})
-		}, "json");
-	});
-})
+//$(function () {
+	
+	$("#searchbutton").on("Click", function (e) {
+	$.get( "https://api.github.com/users/"+$(#user).innertext+"/repos"(, function( data ) {
+		$( "#result_page" ).html( data );
+		    alert( "Load was performed." );
+		});
+		
+//	});
+//});
