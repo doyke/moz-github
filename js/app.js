06811c763ae1ab0cf9bc66d283394dced08e538f
Gianmarco Di Lella <giammyjet@gmail.com>
2014-02-25 21:25:45 +0100

    update app.js
    cancellati i commenti inutili

--- js/app.js
+++ js/app.js
@@ -1,10 +1,5 @@
-//$(function () {
-	
-	$("#searchbutton").on("Click", function (e) {
+$("#searchbutton").on("Click", function (e) {
 	$.get( "https://api.github.com/users/"+$(#user).innertext+"/repos"(, function( data ) {
 		$( "#result_page" ).html( data );
 		    alert( "Load was performed." );
 		});
-		
-//	});
-//});
